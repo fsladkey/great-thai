fe493eabb94461c8252e23554c6a85f8a15a7ab3
Fred Sladkey <fsladkey@gmail.com>
2017-02-26 14:36:12 -0500

    Add query helper functions

---
+++ query_helpers.py
@@ -0,0 +1,28 @@
+from db_connection import fetch_all
+
+
+def restaurants_where(where={}, limit=10):
+    where_str = " AND ".join(["{} = %({})s".format(col, col) for col in where])
+    return fetch_all("""
+    SELECT
+      *
+    FROM
+      restaurants
+    WHERE
+      {}
+    LIMIT
+      10
+    """.format(where_str), where)
+
+
+def all_restaurants(limit=10):
+    return fetch_all("""
+    SELECT
+      *
+    FROM
+      restaurants
+    LIMIT
+      10
+    """)
+
+print(all_restaurants())
