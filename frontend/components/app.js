fdfe142391e6d2c57ae0ca7ccebeeca3fd72b6d0
Fred Sladkey <fsladkey@gmail.com>
2017-02-26 17:20:43 -0500

    Create app component

---
+++ frontend/components/app.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import { fetchCuisines } from '../api_util'
+import CuisineList from './cuisine_list'
+import CuisineDetail from './cuisine_detail'
+import Loader from './loader'
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      cuisines: [],
+      selected: "Thai",
+      fetching: true
+    };
+  }
+
+  componentDidMount() {
+    fetchCuisines().then(cuisines =>
+      this.setState({ cuisines: cuisines.sort(), fetching: false })
+    );
+  }
+
+  setSelected = (selected) => {
+    this.setState({ selected });
+  }
+
+  render() {
+    const { fetching, cuisines, selected } = this.state;
+    if (fetching) return <Loader />;
+
+    return (
+      <main>
+        <h1>{ selected }</h1>
+        <CuisineList
+          cuisines={ cuisines }
+          setSelected={ this.setSelected }
+          selected={ selected }
+          />
+        <CuisineDetail cuisine={ selected }/>
+      </main>
+    );
+  }
+}
